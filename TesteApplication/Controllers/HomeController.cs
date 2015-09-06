5ee2cf3271ae306712ca63745beb3dbc71f6250e
Jaer86 <scorcar@hotmail.com>
2015-09-06 12:38:13 -0500

    Primer commit local

---
+++ TesteApplication/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TesteApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                ViewBag.EstaAtenticado = "Esta autenticado";
+            }
+
+            Console.WriteLine();
+
+            return View();
+        }
+    }
+}
